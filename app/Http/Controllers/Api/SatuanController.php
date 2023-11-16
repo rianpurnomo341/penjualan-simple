327c40884038b2c128e31f5f629a3532c766702c
rochim1 <muhammad.0206@students.amikom.ac.id>
2023-11-16 19:41:36 +0700

    count barang jadi

--- app/Http/Controllers/Api/SatuanController.php
+++ app/Http/Controllers/Api/SatuanController.php
@@ -7,16 +7,17 @@ use App\Http\Resources\ApiResource;
 use App\Models\Satuan;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SatuanController extends Controller
 {
     public function index()
     {
         try {
-            $satuan = Satuan::withcount('barang')
-            ->select('satuan.*', DB::raw('SUM(barang.qty) as total_stock'))
+            $satuan = Satuan::select('satuan.*', DB::raw('SUM(barang.qty) as total_stock'))
             ->leftJoin('barang', 'satuan.id_satuan', '=', 'barang.satuan_id')
-            ->groupBy('satuan.id_satuan')
+            ->groupBy('satuan.id_satuan','satuan.nama_satuan','satuan.ket_satuan','satuan.created_at','satuan.updated_at')
+            ->withcount('barang as count_barang')
             ->get();
             return new ApiResource(true, 'Berhasil Menampilkan Data', $satuan);
         } catch (QueryException $e) {
